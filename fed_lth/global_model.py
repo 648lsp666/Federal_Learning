8ce8defbca00c12cf3fc59aebde5459e5b3e0ed7
mkldy12138 <122950257+mkldy12138@users.noreply.github.com>
2024-05-18 12:53:41 +0800

    划水老安
    更新local model

--- fed_lth/global_model.py
+++ fed_lth/global_model.py
@@ -4,30 +4,15 @@ import torch
 import numpy as np
 from torchvision import datasets, transforms
 
-from utils import *
-from pruning_utils import *
-from pruning_utils import regroup
 from conf import conf
 
 
 class Global_model(object):
   # 初始化的变量在这里面
   def __init__(self) -> None:
-    self.device = torch.device(conf['global_dev'])
-    self.model, self.train_loader, self.val_loader, self.test_loader = setup_model_dataset(conf)
-    self.model.to(self.device)
-    self.decreasing_lr = list(map(int, conf['decreasing_lr'].split(',')))
-    self.optimizer = torch.optim.SGD(self.model.parameters(), conf['lr'], momentum=conf['momentum'],
-                                     weight_decay=conf['weight_decay'])
-    self.scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=self.decreasing_lr, gamma=0.1)
-
-    print(self.model.normalize)
-    self.global_epoch = conf['global_epoch']
-    self.node_num = conf['num_client']  #可以声明对象时按实际需求设置
-    self.sub_epoch = conf['local_epoch']
-
-    self.load_pretrained()
-  #聚合函数
+    self.init_model=torch.load(conf['init_model'])
+  
+  #聚合函数 
   def aggregate(self):
     pass
 
@@ -39,37 +24,6 @@ class Global_model(object):
   # 掩码结构化重组@mk
   def regroup(self):
     pass
-
-  # 载入预训练模型(彩票)
-  def load_pretrained(self):
-    initalization = torch.load(conf['pretrained'], map_location=self.device)
-    if 'init_weight' in initalization.keys():
-      print('loading from init_weight')
-      initalization = initalization['init_weight']
-    elif 'state_dict' in initalization.keys():
-      print('loading from state_dict')
-      initalization = initalization['state_dict']
-
-    loading_weight = extract_main_weight(initalization, fc=True, conv1=True)
-    new_initialization = self.model.state_dict()
-    if not 'normalize.std' in loading_weight:
-      loading_weight['normalize.std'] = new_initialization['normalize.std']
-      loading_weight['normalize.mean'] = new_initialization['normalize.mean']
-
-    if not (args.prune_type == 'lt' or args.prune_type == 'trained'):
-      keys = list(loading_weight.keys())
-      for key in keys:
-        if key.startswith('fc') or key.startswith('conv1'):
-          del loading_weight[key]
-
-      loading_weight['fc.weight'] = new_initialization['fc.weight']
-      loading_weight['fc.bias'] = new_initialization['fc.bias']
-      loading_weight['conv1.weight'] = new_initialization['conv1.weight']
-
-    print('*number of loading weight={}'.format(len(loading_weight.keys())))
-    print('*number of model weight={}'.format(len(model.state_dict().keys())))
-    model.load_state_dict(loading_weight)
-
 
 if __name__=='__main__':
   global_model=Global_model()
--- fed_lth/local_model.py
+++ fed_lth/local_model.py
@@ -8,6 +8,10 @@ from dataset import get_dataset
 from conf import conf
 import time
 import torchvision
+from torchvision.models import resnet18
+import torch_pruning as tp
+import torch.nn as nn
+import torch.optim as optim
 
 class Local_model(object):
   def __init__(self, id):
@@ -22,22 +26,108 @@ class Local_model(object):
     inputs = torch.randn(16, 3, 224, 224)  # 16张 224x224 的图片
     labels = torch.randint(0, conf['n_class']-1, (16,))  # 16个标签，范围在0-9之间 10分类任务
     # 测时间 数据集数量/16*time
-      # self.model.train()
+    self.model.train()
     # 训练
+    # 创建损失函数和优化器
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9)
 
-    return 'time'
+    # 计算时间
+    start_time = time.time()
+    # 前向传播
+    outputs = self.model(inputs)
+    loss = criterion(outputs, labels)
+
+    # 反向传播和优化
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    end_time = time.time()
+
+    # 计算并打印时间
+    elapsed_time = end_time - start_time
+    final_time = len(self.test_data)*elapsed_time/16
+
+    return final_time
 
   def s_prune(self,ratio):
     # 剪枝函数 输入是剪枝率
-    pass
+    class MySlimmingImportance(tp.importance.Importance):
+      def __call__(self, group, **kwargs):
+        group_imp = []
+        for dep, idxs in group:
+          layer = dep.target.module
+          prune_fn = dep.handler
+          if isinstance(layer, (nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d)) and layer.affine:
+            importance_scores = torch.abs(layer.weight.data)  # 使用 L1 范数计算重要性
+            group_imp.append(importance_scores)
+        if len(group_imp) == 0: return None
+        group_imp = torch.stack(group_imp, dim=0).mean(dim=0)
+        return group_imp
+
+    class MySlimmingPruner(tp.pruner.MetaPruner):
+      def regularize(self, model, reg):
+        for m in model.modules():
+          if isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d)) and m.affine:
+            m.weight.grad.data.add_(reg * torch.sign(m.weight.data))
+
+    imp = MySlimmingImportance()
+
+    # 忽略最后的分类层
+    ignored_layers = []
+    for m in self.model.modules():
+      if isinstance(m, torch.nn.Linear) and m.out_features == 1000:
+        ignored_layers.append(m)
+
+    # 初始化剪枝器
+    example_inputs = torch.randn(1, 3, 224, 224)
+    iterative_steps = 1
+    pruner = MySlimmingPruner(
+      self.model,
+      example_inputs,
+      importance=imp,
+      iterative_steps=iterative_steps,
+      ch_sparsity=ratio,
+      ignored_layers=ignored_layers,
+    )
+
+    # 定义损失函数和优化器（可更改）
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9)
+
+    # 模拟训练数据
+    inputs = torch.randn(16, 3, 224, 224)  # 16张 224x224 的图片
+    labels = torch.randint(0, 1000, (16,))  # 16个标签，范围在0-999之间
+
+    # 进行稀疏训练
+    for epoch in range(5):  # 训练5个epoch
+      self.model.train()
+      optimizer.zero_grad()
+      outputs = self.model(inputs)
+      loss = criterion(outputs, labels)
+      loss.backward()
+      pruner.regularize(self.model, reg=1e-5)  # 稀疏化
+      optimizer.step()
+
+    pruner.step()
 
   # 测试剪枝率，输入是时间阈值 单位秒
   def prune_ratio(self,time_T):
-    # 循环
-    # 重置模型 self.model
-    #prune(ratio) 剪枝 
-    #self.time_test() 测时间
-    return 'prune_ratio'
+    model = self.model
+    start = 0.0
+    end = 1.0
+    step = 0.1
+    current = start
+    while current <= end:
+      self.model = model
+      self.s_prune(current)
+      if self.time_test() <= time_T:
+        self.model = model
+        prune_ratio = current
+        break
+      current += step
+    return prune_ratio
     
 
 
@@ -46,5 +136,17 @@ if __name__=='__main__':
   local=Local_model(1)
   local.model=torchvision.models.resnet18()
   train_time=local.time_test()
+  print(train_time)
   time_T=1000
   prune_ratio=local.prune_ratio(time_T)
+  print(prune_ratio)
+
+  #剪枝测试
+  example_inputs = torch.randn(1, 3, 224, 224)
+  base_macs, base_nparams = tp.utils.count_ops_and_params(local.model, example_inputs)
+  # 执行剪枝
+  local.s_prune(0.5)
+  # 剪枝后计算模型参数和计算量
+  macs, nparams = tp.utils.count_ops_and_params(local.model, example_inputs)
+  print("参数数量: {:.2f} M => {:.2f} M".format(base_nparams / 1e6, nparams / 1e6))
+  print("计算量: {:.2f} G => {:.2f} G".format(base_macs / 1e9, macs / 1e9))
